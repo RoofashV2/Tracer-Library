eca377ba11b502ca06d689554b32151e28d72ef6
RoofashV2 <126738777+RoofashV2@users.noreply.github.com>
2023-04-08 21:26:57 -0400

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,3 +1,9 @@
+Drawing = getfenv().Drawing do
+	if Drawing == nil then
+		error("Executor not supported")
+	end
+end
+
 local HttpService = game:GetService("HttpService")
 local RunService = game:GetService("RunService")
 local Players = game:GetService("Players")
@@ -17,25 +23,18 @@ end
 Module.new = function(highlightenabled)
 	local self = setmetatable({}, Tracer)
 
-	local UI = Instance.new("ScreenGui")
-	UI.ResetOnSpawn = false
-	
-	if game.CoreGui then
-		UI.Parent = game.CoreGui
+	local GUIlocation = (game.CoreGui or LocalPlayer.PlayerGui)
+	if GUIlocation:FindFirstChild("ESPholder") then
+		self.Holder = GUIlocation:FindFirstChild("ESPholder")
 	else
-		UI.Parent = LocalPlayer.PlayerGui
+		self.Holder = Instance.new("ScreenGui", GUIlocation)
+		self.Holder.ResetOnSpawn = false
+		self.Holder.Name = "ESPholder"
 	end
 
-	local ViewportFrame = Instance.new("ViewportFrame", UI)
-	ViewportFrame.Size = UDim2.new(1, 0, 1, 0)
-	ViewportFrame.Transparency = 1
-	ViewportFrame.CurrentCamera = workspace.CurrentCamera
-
-	self["UI"] = UI
-	self["ViewportFrame"] = ViewportFrame
-	self.HighlightEnabled = highlightenabled do
-		if highlightenabled == nil then
-			self.HighlightEnabled = true
+	self.HighlightEnabled = true do
+		if highlightenabled ~= nil then
+			self.HighlightEnabled = highlightenabled
 		end
 	end
 
@@ -48,55 +47,69 @@ function Tracer:Bind(object, highlightobject)
 		assert(self.Disconnected, "Tracer:: Disconnected, cannot use functions.")
 		return rawget(self, index)
 	end
-	
+
 	self.Disconnected = false
 	self.Running = true
 	self.Target = object
 	self.TracerId = HttpService:GenerateGUID(false)
 	self.Color = Color3.fromRGB(255, 255, 255)
-	
-	local part = Instance.new("Part", self["ViewportFrame"])
-	part.Color = self.Color
-	part.Material = Enum.Material.Neon
-	part.Anchored = true
-	part.CanCollide = false
-	
+	self.Label = Drawing.new("Text")
+
+	local Line = Drawing.new("Line")
+	Line.Thickness = 2
+	Line.Transparency = 1
+	Line.ZIndex = 1
+
+	local Label = self.Label
+	Label.Visible = true
+	Label.Center = true
+	Label.Outline = true
+	Label.Size = 30
+	Label.Font = 2
+	Label.Text = ""
+	Label.Color = self.Color
+
 	if self.HighlightEnabled then
-		self.Highlight = Instance.new("Highlight", (highlightobject or object))
+		self.Highlight = Instance.new("Highlight", self.Holder)
 		self.Highlight.FillTransparency = 1
 		self.Highlight.OutlineColor = self.Color
+		self.Highlight.Adornee = self.Target
+	else
+		self.Highlight:Destroy()
 	end
 
-	self.TracerPart = part
 	function self:CleanUp()
 		if not self.Disconnect then
 			error("CleanUp can only be called once disconnected.")
 			return
 		end
-		self.Highlight:Destroy()
-		part:Destroy()
-		self.UI:Destroy()
-	end
-	
-	RunService:BindToRenderStep(self.TracerId, Enum.RenderPriority.First.Value, function()
-		if not self.Running or self.Disconnected then
-			return
+		if self.HighlightEnabled then
+			self.Highlight:Destroy()
 		end
-		
-		part.Color = self.Color
+		Line:Remove()
+		Label:Remove()
+	end
+
+	RunService:BindToRenderStep(self.TracerId, Enum.RenderPriority.First.Value, function()
+		local CurrentCamera = workspace.CurrentCamera
+		local To, OnScreen = CurrentCamera:WorldToViewportPoint(self.Target.Position)
+
 		if self.Highlight then
 			self.Highlight.OutlineColor = self.Color
 		end
-		
-		local target = self.Target
-		local pos = (Root.Position - object.Position)
-		local lookVector = pos.Unit
-		local distance = pos.Magnitude
-		part.CFrame = CFrame.new(Vector3.new(0, 0, 0), -lookVector)
-		part.Position = target.Position + (lookVector * distance/2)
-		part.Size = Vector3.new(0.05, 0.05, (Root.Position - target.Position).Magnitude)
+
+		if self.Label.Text ~= "" then
+			Label.Position = Vector2.new(To.X, To.Y - 35)
+			Label.Visible = OnScreen
+			Label.Color = self.Color
+		end
+
+		Line.Color = self.Color
+		Line.Visible = OnScreen
+		Line.From = Vector2.new(CurrentCamera.ViewportSize.X / 2, CurrentCamera.ViewportSize.Y)
+		Line.To = Vector2.new(To.X, To.Y)
 	end)
-	
+
 	return self
 end
 
@@ -109,31 +122,8 @@ function TracerFunctions:Pause()
 end
 
 function TracerFunctions:SetText(text)
-	if self.Target:FindFirstChild(self.TracerId) then
-		local BillboardGui = self.Target:FindFirstChild(self.TracerId)
-		BillboardGui.TextLabel.Text = text
-	else
-		local BillboardGui = Instance.new("BillboardGui")
-		BillboardGui.Active = true
-		BillboardGui.AlwaysOnTop = true
-		BillboardGui.ClipsDescendants = true
-		BillboardGui.LightInfluence = 1
-		BillboardGui.Size = UDim2.new(5, 0, 1.3, 0)
-		BillboardGui.StudsOffset = Vector3.new(0, 1, 0)
-		BillboardGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-		BillboardGui.Parent = self.Target
-		BillboardGui.Name = self.TracerId
-
-		local TextLabel = Instance.new("TextLabel")
-		TextLabel.BackgroundTransparency = 1
-		TextLabel.Font = Enum.Font.SourceSans
-		TextLabel.Size = UDim2.new(1, 0, 1, 0)
-		TextLabel.Text = text
-		TextLabel.TextColor3 = self.Color
-		TextLabel.TextScaled = true
-		TextLabel.TextSize = 14
-		TextLabel.TextWrapped = true
-		TextLabel.Parent = BillboardGui
+	if self.Label then
+		self.Label.Text = text
 	end
 end
 
@@ -164,7 +154,7 @@ function TracerFunctions:SetColor(color)
 			return
 		end
 	end
-	
+
 	error("Expected Color, got " .. type(color) .. ".")
 end
 
